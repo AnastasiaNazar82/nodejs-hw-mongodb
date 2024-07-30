b1f2f835d0d8f3ce0d8dd0e8cb5d3ff71c4f023b
AnastasiaNazar82 <148636770+AnastasiaNazar82@users.noreply.github.com>
2024-07-30 17:51:09 +0300

    add

--- src/utils/parseFilterParams.js
+++ src/utils/parseFilterParams.js
@@ -2,7 +2,7 @@ const parseContactType = (contactType) => {
   const isString = typeof contactType === 'string';
   if (!isString) return;
   const isContactType = (contactType) =>
-    ['home', 'personal'].includes(contactType);
+    ['work', 'home', 'personal'].includes(contactType);
 
   if (isContactType(contactType)) return contactType;
 };
